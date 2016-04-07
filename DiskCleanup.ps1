298bc3705418c9c1cca14944ec721380ff186f4e
digital-edge-it <andrew@digital-edge-it.com>
2016-04-07 09:09:22 -0500

    Created output options and started to add folders to clean up

--- DiskCleanup.ps1
+++ DiskCleanup.ps1
@@ -1,1 +1,16 @@
-Write-Host "test"
+$StartDiskFree = Get-WmiObject Win32_LogicalDisk -Filter "DeviceID='C:'" | Foreach-Object {$_.FreeSpace}
+
+#Clear Recycle Bin
+Remove-Item 'C:\$RecycleBin\*' -Recurse -WhatIf
+
+Remove-Item 'C:\Windows\Installer\*' -Recurse
+
+#$EndDiskFree = "15299767808"
+$EndDiskFree = Get-WmiObject Win32_LogicalDisk -Filter "DeviceID='C:'" | Foreach-Object {$_.FreeSpace}
+$Savings = $StartDiskFree - $EndDiskFree
+
+#Turns values into human readable for output
+$StartDiskFree = [math]::round($StartDiskFree / 1Gb, 2)
+$EndDiskFree = [math]::round($EndDiskFree / 1Gb, 2)
+$Savings = [math]::round($Savings / 1Gb, 2)
+Write-Host "Space Free at Start: $StartDiskFree GB`r`nSpace Free at End $EndDiskFree GB`r`nSpace Reclaimed $Savings GB"
