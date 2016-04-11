6c9834b310f77ff1b32db42fba2b71554a80a66a
digital-edge-it <andrew@digital-edge-it.com>
2016-04-11 09:52:03 -0500

    Finished adding items to clean up

--- DiskCleanup.ps1
+++ DiskCleanup.ps1
@@ -1,15 +1,33 @@
+#Functions
+function Clean
+{
+    Remove-Item $args[0] -Recurse -Force -ErrorAction SilentlyContinue
+}
+
+#Main
 $StartDiskFree = Get-WmiObject Win32_LogicalDisk -Filter "DeviceID='C:'" | Foreach-Object {$_.FreeSpace}
 
-#Clear Recycle Bin
-Remove-Item 'C:\$RecycleBin\*' -Recurse -Verbose -ErrorAction SilentlyContinue
+#Clear Windows Installer Directory
+Clean 'C:\Windows\Installer\*'
 
-Remove-Item 'C:\Windows\Installer\*' -Recurse -Verbose -ErrorAction SilentlyContinue
+#Clear Windows Temp Folder
+Clean 'C:\Windows\temp\*'
 
+#Clear Windows Logs
+Clean 'C:\Windows\logs\*'
 
+#Clear Windows Update Cache
+Stop-Service wuauserv
+Clean 'C:\Windows\SoftwareDistribution\*'
+Start-Service wuauserv
+
+#Clear Temp Folders
+Clean 'C:\temp\*'
+clean $env:temp
 
 #$EndDiskFree = "15299767808"
 $EndDiskFree = Get-WmiObject Win32_LogicalDisk -Filter "DeviceID='C:'" | Foreach-Object {$_.FreeSpace}
-$Savings = $StartDiskFree - $EndDiskFree
+$Savings = $EndDiskFree - $StartDiskFree
 
 #Turns values into human readable for output
 $StartDiskFree = [math]::round($StartDiskFree / 1Gb, 2)
